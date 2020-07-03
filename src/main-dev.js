a15572c0928429d1616caeaae32a271448269e18
lrvings <1018610468@qq.com>
2020-07-03 23:33:52 +0800

    update optimized

---
+++ src/main-dev.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import './plugins/element.js'
+import 'assets/css/global.css'
+import axios from 'axios'
+import TreeTable from 'vue-table-with-tree-grid'
+import VueQuillEditor from 'vue-quill-editor'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+// 配置请求
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+
+//* 保证拥有获取数据的权限
+axios.interceptors.request.use(config => {
+  NProgress.start()
+  config.headers.Authorization = window.sessionStorage.getItem('tokenid')
+  return config
+})
+axios.interceptors.response.use(config => {
+  NProgress.done()
+  return config
+})
+
+Vue.prototype.$http = axios
+
+Vue.config.productionTip = false
+
+Vue.component('tree-table', TreeTable)
+// 富文本
+Vue.use(VueQuillEditor)
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,49 @@
+const path = require('path')
+
 module.exports = {
-  configureWebpack: {
-    resolve: {
-      alias: {
-        components: '@/components',
-        commons: 'components/common',
-        common: '@/common',
-        assets: '@/assets'
-      }
-    }
-  },
-  lintOnSave: false
+  lintOnSave: true,
+  chainWebpack(config) {
+    config.resolve.alias
+      .set('common', path.join(__dirname, './src/common'))
+      .set('assets', path.join(__dirname, './src/assets'))
+      .set('components', path.join(__dirname, './src/components'))
+      .set('commons', path.join(__dirname, './src/components/common'))
+
+    // 发布模式
+    config.when(process.env.NODE_ENV === 'production', config => {
+      config
+        .entry('app')
+        .clear()
+        .add('./src/main-prod.js')
+
+      config.set('externals', {
+        vue: 'Vue',
+        'vue-router': 'VueRouter',
+        axios: 'axios',
+        lodash: '_',
+        echarts: 'echarts',
+        nprogress: 'NProgress',
+        'vue-quill-editor': 'VueQuillEditor'
+      })
+
+      config.plugin('html').tap(args => {
+        args[0].isProd = true
+        return args
+      })
+    })
+
+    // 开发模式
+    config.when(process.env.NODE_ENV === 'development', config => {
+      config
+        .entry('app')
+        .clear()
+        .add('./src/main-dev.js')
+
+      config.plugin('html').tap(args => {
+        args[0].isProd = false
+        return args
+      })
+    })
+  }
+
 }
